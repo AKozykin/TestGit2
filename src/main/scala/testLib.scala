1f6c83e641556e31fbe797c9e237f871cef91bcb
AKozykin <AKozykin@korusconsulting.ru>
2019-01-11 12:02:56 +0300

    Test Artifact

---
+++ src/main/scala/testLib.scala
@@ -0,0 +1,8 @@
+package TestGit.aid
+
+object testLib {
+  def main(): Int = {
+    val a = 112
+    return a
+  }
+}
---
+++ src/test/scala/TestGitPac/TestGitObj543.scala
@@ -0,0 +1,12 @@
+package sample
+
+
+
+object TestLive {
+  def main(args: Array[String]): Unit = {
+
+    System.out.print("Happy Birthday")
+
+  }
+}
+
